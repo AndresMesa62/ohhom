8792e1eb3e31e2e78a5f1701fd3973985479fe38
AndresMesa62 <andresmesa-99@hotmail.com>
2023-03-21 01:21:15 -0500

    listo contacto y listo locations

--- ohhom/views.py
+++ ohhom/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from agency.models import Property
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 def home(request):
     properties=Property.objects.all().filter(is_avalible=True)
-
     context = {
         'properties':properties,
     }
+    
     return render(request,'home.html',context)
+
